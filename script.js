015fb16e63add0fcf9a198b4110891c6b5d0a928
EugeneSemivolos <semivolos.evgen1@gmail.com>
2022-07-06 10:09:40 +0300

    completed 2-nd Practika

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
-console.log(typeof numberOfFilms);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -11,9 +10,22 @@ const personalMovieDB = {
   private: false,
 };
 
-for (let i = 0; i < numberOfFilms; i++) {
-  const nameFilm = prompt(`Введите ${i + 1}-й просмотренный фильм: `);
-  const rating = prompt("На сколько оцените его?");
+if (personalMovieDB.count < 10) alert('Просмотренно довольно мало фильмов!');
+  else if (personalMovieDB.count < 30) alert('Вы классический зритель!');
+    else alert('Вы - киноман!');
+alert('Вспомните последние 3 просмотренных фильма');
+for (let i = 0; i < 3; i++) {
+  let nameFilm = prompt(`Введите ${i + 1}-й просмотренный фильм: `);
+
+  while (!nameFilm && nameFilm.length > 50) {
+    nameFilm = prompt(`Введите ${i + 1}-й просмотренный фильм: `);
+  }
+
+  let rating = prompt('На сколько оцените его?');
+
+  while (!rating) {
+    rating = prompt('На сколько оцените его?');
+  }
   personalMovieDB.movies[nameFilm] = rating;
 }
 
